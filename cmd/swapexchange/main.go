087f4dbbbc5248f5a99658379252f87f6866b519
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 23:14:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/swapexchange/main.go
@@ -0,0 +1,23 @@
+// cmd/swapexchange/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "swapexchange/internal/swapexchange"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := swapexchange.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
